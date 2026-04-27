d76d43e6529a02807597cc7aa89861e609b60361
liner <linerlnder@gmail.com>
2026-04-26 22:15:19 -0500

    menu interactivo

--- menu.sh
+++ menu.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#!/bin/bash
 
+while true; do
 clear
-
 echo "=================================="
 echo "        🔥 SCRIPT KIRA 🔥"
 echo "=================================="
@@ -18,17 +17,23 @@ case $op in
 1)
   echo "Tu IP es:"
   curl -s ifconfig.me
+  echo ""
+  read -p "Presiona ENTER para continuar..."
   ;;
 2)
   free -h
+  read -p "Presiona ENTER para continuar..."
   ;;
 3)
   apt update -y && apt upgrade -y
+  read -p "Presiona ENTER para continuar..."
   ;;
 0)
   exit
   ;;
 *)
   echo "Opcion invalida"
+  sleep 1
   ;;
 esac
+done

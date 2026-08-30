0f1e6f411bfb6234e61d1dabe5495cc0ffa0a3f0
LinerLander <linerlander@gmail.com>
2026-08-30 18:05:24 -0500

    version 2

--- modules/user_clear.sh
+++ modules/user_clear.sh
@@ -18,7 +18,7 @@ PORT=$(grep -i "^Port" /etc/ssh/sshd_config | awk '{print $2}' | head -n1)
 while true; do
 clear
 echo -e "${D}╔═══════════════════════════════════════════════════════════════════════╗${N}"
-echo -e "${D}║${Y}                     🗑️   PANEL DE ELIMINACIÓN DE USUARIOS                 ${D}║${N}"
+echo -e "${D}║${Y}                     🗑️   PANEL DE ELIMINACIÓN DE USUARIOS              ${D}║${N}"
 echo -e "${D}╠═══════════════════════════════════════════════════════════════════════╣${N}"
 printf "${D}║${N} ${C}%-4s %-16s %-12s %-8s %-9s %-15s${N} ${D}  ║${N}\n" "ID" "USUARIO" "PASS" "PUERTO" "LÍMITE" "EXPIRACIÓN"
 echo -e "${D}╠═══════════════════════════════════════════════════════════════════════╣${N}"
@@ -121,7 +121,7 @@ if [ $i -eq 1 ]; then
 fi
 
 echo -e "${D}╠═══════════════════════════════════════════════════════════════════════╣${N}"
-echo -e "${D}║${N} ${R}[0] REGRESAR AL MENÚ PRINCIPAL${N}                                        ${D} ║${N}"
+echo -e "${D}║${N} ${R}[0] REGRESAR AL MENÚ PRINCIPAL${N}                                        ${D}║${N}"
 echo -e "${D}╚═══════════════════════════════════════════════════════════════════════╝${N}"
 echo ""
 read -p " ► Selecciona el ID del usuario a eliminar: " selection
